11dd92e889179538c2ecb260a743580db00225a7
treeforever <treeforever7@gmail.com>
2018-08-02 22:50:48 -0400

    add logo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='header-container'>
     <span class="header">Fabulous Stats</span>
+    <img src='../../static/umbrella.png' class="logo"/>
   </div>
 </template>
 
@@ -23,12 +24,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .header-container {
+  display: flex;
+  flex-direction: column;
   text-align: center;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
 }
 .header {
+
   font-size: 5em;
   font-family: 'avenir';
   font-weight: 700;
   color: #1EA896;
+}
+.logo {
+  width: 10em;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
